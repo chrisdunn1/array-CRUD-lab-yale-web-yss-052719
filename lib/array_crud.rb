7f9d17461b0e23c7c126c82cd774219c12dd8af1
Christopher Dunn <christopherdunnlaw@gmail.com>
2019-05-12 00:31:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,12 +28,14 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array.shift[0]
+  array.shift(0)
 end
 
 
 
 def retrieve_element_from_index(array, index_number)
+
+  
 
 end
 
